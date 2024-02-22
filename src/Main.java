41b8d77c5ac6daa10b32959d9f9309369accf64d
shahovs <shahov12345@yandex.ru>
2024-02-22 23:32:39 +0100

    v 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+// 3 version
 // 2 version
 // 1 version
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("String2");
+        System.out.println("String3");
     }
 }
